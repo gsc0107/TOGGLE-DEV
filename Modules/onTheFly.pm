0a6e83466734abf6c016b4ed416dc70f2e53fc88
Francois Sabot <francois.sabot@ird.fr>
2016-02-17 09:50:58 +0100

    Removing anything after a space. E.g a samtoolsview 1 will become samtoolsView

--- Modules/onTheFly.pm
+++ Modules/onTheFly.pm
@@ -355,7 +355,7 @@ sub generateGraphviz
 	##DEBUG print $soft,"-->";
 	##DEBUG toolbox::exportLog("DEBUG : $0 : onTheFly::generateGraphviz, step = $step, soft = $soft.\n",2);
 
-	#Removing anything after a space. E.g a samtoolsview 1 will become samtoolsView2
+	#Removing anything after a space. E.g a samtoolsview 1 will become samtoolsView
 	$soft =~ s/ *+$//;
 	
 	$input=$$hashInOut{$soft}{"IN"};
