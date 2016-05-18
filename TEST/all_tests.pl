003c17425bf7cc0291aaf377fc7dd0a1f4c21220
christine <christine.tranchant@ird.Fr>
2016-05-18 10:51:10 +0200

    Add 1rst tests toggleGenerator - pairedOneIndividu

--- TEST/all_tests.pl
+++ TEST/all_tests.pl
@@ -54,14 +54,18 @@
 
 use strict;
 use warnings;
+use Test::More 'no_plan'; 
+use Test::Deep;
 
 
 ######################
 ## SUB
 ######################
 
-sub sedFunction(my $file):
+sub sedFunction
 {
+    my $file=$_[0];
+    
     # Change the TOGGLE addaptator configuration file
     my $sed="sed -i -e 's|-b ADAPTATOR1REVERSE -B ADAPTATOR1REVERSE|-b GATCGGAAGAGCACACGTCTGAACTCCAGTCACATCACGATCTCGTATGCCGTCTTCTGCTTG  -B GATCGGAAGAGCACACGTCTGAACTCCAGTCACATCACGATCTCGTATGCCGTCTTCTGCTTG|' ". $file;
     #print $sed."\n\n";
@@ -81,22 +85,26 @@ sub sedFunction(my $file):
 #####################
 ## PATH for datas test
 #####################
-#fastq
+#dna fastq
 my $dataFastqpairedOneIndividuArcad = "../DATA/testData/fastq/pairedOneIndividuArcad";
 my $dataFastqpairedTwoIndividusGzippedIrigin = "../DATA/testData/fastq/pairedTwoIndividusGzippedIrigin";
 my $dataFastqpairedTwoIndividusIrigin = "../DATA/testData/fastq/pairedTwoIndividusIrigin";
 my $dataFastqsingleOneIndividuIrigin = "../DATA/testData/fastq/singleOneIndividuIrigin";
 my $dataFastqsingleTwoIndividuIrigin = "../DATA/testData/fastq/singleTwoIndividuIrigin";
-#rnaseq
+
+#rnaseq fastq
 my $dataRNAseqPairedOneIndividu = "../DATA/testData/rnaseq/pairedOneIndividu";
 my $dataRNAseqSingleOneIndividu = "../DATA/testData/rnaseq/singleOneIndividu";
+
 #samBam
 my $dataSamBamOneBam = "../DATA/testData/samBam/oneBam";
 my $dataSamBamOneSam = "../DATA/testData/samBam/oneSam";
 my $dataSamBamTwoBamsIrigin = "../DATA/testData/samBam/twoBamsIrigin";
+
 #vcf
 my $dataVcfSingleVCF = "../DATA/testData/vcf/singleVCF";
 my $dataVcfVcfForRecalibration = "../DATA/testData/vcf/vcfForRecalibration";
+
 # references files
 my $dataRefIrigin = "../DATA/Bank/referenceIrigin.fasta";
 my $dataRefArcad = "../DATA/Bank/referenceArcad.fasta";
@@ -117,16 +125,7 @@ print "#### Copy conf file SNPdiscoveryPaired : $cmd\n";
 system($cmd) and die ("#### ERROR COPY CONFIG FILE: $cmd\n");     # Copy into TEST
 
 # Change the TOGGLE addaptator configuration file
-
 sedFunction($fileSNPPairedNoSGE);
-exit;
-
-my $sed="sed -i -e 's|-b ADAPTATOR1REVERSE -B ADAPTATOR1REVERSE|-b GATCGGAAGAGCACACGTCTGAACTCCAGTCACATCACGATCTCGTATGCCGTCTTCTGCTTG  -B GATCGGAAGAGCACACGTCTGAACTCCAGTCACATCACGATCTCGTATGCCGTCTTCTGCTTG|' ". $fileSNPPairedNoSGE;
-#print $sed."\n\n";
-system($sed) and die ("#### ERROR  SED COMMAND: $sed\n");
-$sed="sed -i -e 's|-b ADAPTATOR1FORWARD -B ADAPTATOR1FORWARD|-b GTTCGTCTTCTGCCGTATGCTCTAGCACTACACTGACCTCAAGTCTGCACACGAGAAGGCTAG -B GTTCGTCTTCTGCCGTATGCTCTAGCACTACACTGACCTCAAGTCTGCACACGAGAAGGCTAG|' ". $fileSNPPairedNoSGE;
-#print $sed."\n\n";
-system($sed) and die ("#### ERROR  SED COMMAND: $sed\n");
 
 #run with new fileconf SNPdiscoveryPaired for no SGE mode to pairedOneIndividuArcad
 my $testingDir="../DATA-TEST/pairedOneIndividuArcad";
@@ -137,28 +136,30 @@ my $runcmd = "toggleGenerator.pl -c ".$fileSNPPairedNoSGE." -d ".$dataFastqpaire
 system("$runcmd") and die "#### ERROR : Can't run TOGGLE for pairedOneIndividuArcad";
 
 # check final results
+my $observedOutput = `ls $testingDir/finalResults`;
+my @observedOutput = split /\n/,$observedOutput;
+my @expectedOutput = ('arcad1.GATKSELECTVARIANT.vcf','arcad1.GATKSELECTVARIANT.vcf.idx');
+
+is_deeply(\@observedOutput,\@expectedOutput,'toggleGenerator - pairedOneIndividu');
+
+exit;
 
 
+# expected output content
+#$observedOutput=`tail -n 1 $vcfOut`;
+#chomp $observedOutput;
+#$expectedOutput="2233572	145	.	A	G	54.74	.	AC=2;AF=1.00;AN=2;DP=2;Dels=0.00;FS=0.000;HaplotypeScore=0.0000;MLEAC=2;MLEAF=1.00;MQ=49.84;MQ0=0;QD=27.37;SOR=2.303	GT:AD:DP:GQ:PL	1/1:0,2:2:6:82,6,0";
 
-
-
-
-
-
-
-
-
-
-
-
-my $fileSNPSingleIni="../SNPdiscoverySingle.config.txt";          # Path of the SNPdiscoverySingle.config.txt
-my $fileSNPSingle="SNPdiscoverySingleTest.config.txt";
-
-
-my $fileRNASeqIni="../RNASeq.config.txt";          # Path of the RNASeq.config.txt
-my $fileRNASeq="RNASeqTest.config.txt";
-
-
+#is($observedOutput,$expectedOutput,'gatk::gatkUnifiedGenotyper - output content');
+#
+#my $fileSNPSingleIni="../SNPdiscoverySingle.config.txt";          # Path of the SNPdiscoverySingle.config.txt
+#my $fileSNPSingle="SNPdiscoverySingleTest.config.txt";
+#
+#
+#my $fileRNASeqIni="../RNASeq.config.txt";          # Path of the RNASeq.config.txt
+#my $fileRNASeq="RNASeqTest.config.txt";
+#
+#
 
 
 # *** FASTQ ***
