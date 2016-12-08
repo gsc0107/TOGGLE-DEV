8dac7ff6a64365a859c2f0d8211c50d28de67881
Francois Sabot <francois.sabot@ird.fr>
2016-12-08 15:23:25 +0100

    Updating install script

--- TOGGLEinstall.pl
+++ TOGGLEinstall.pl
@@ -292,7 +292,7 @@ system ("cd $INSTALLPATH && tar xzf perlModules.tar.gz && rm -Rf perlModules.tar
 
 #Adding toggle in the user PERL5LIB path
 
-#system ("echo \"\nPERL5LIB=\$PERL5LIB:$MODULES\n\" | cat - >> ~/.bashrc && echo \"\nPATH=\$PATH:$INSTALLPATH\n\" | cat - >> ~/.bashrc && source ~/.bashrc") and die("\nCannot add paths: $!\n");
+system ("echo \"\nPERL5LIB=\$PERL5LIB:$MODULES\n\" | cat - >> ~/.bashrc && echo \"\nPATH=\$PATH:$INSTALLPATH\n\" | cat - >> ~/.bashrc && source ~/.bashrc") and die("\nCannot add paths: $!\n");
 
 print "\n The automatic configuration is finished.\n\nPlease use first the test data as recommended on the GitHub https://github.com/SouthGreenPlatform/TOGGLE.\n\nThanks for using TOGGLE\n";
 
