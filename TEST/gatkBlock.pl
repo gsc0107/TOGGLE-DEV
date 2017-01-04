fdcf0aadc64ef9b102039467212a29ba127ac653
sravel <sebastien.ravel@cirad.fr>
2017-01-04 11:20:43 +0100

    add gatkUnifiedGenotyper

---
+++ TEST/gatkBlock.pl
@@ -0,0 +1,81 @@
+#!/usr/bin/env perl
+
+###################################################################################################################################
+#
+# Copyright 2014 IRD-CIRAD
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/> or
+# write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# You should have received a copy of the CeCILL-C license with this program.
+#If not see <http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.txt>
+#
+# Intellectual property belongs to IRD, CIRAD and South Green developpement plateform
+# Written by Cecile Monat, Christine Tranchant, Ayite Kougbeadjo, Cedric Farcy, Mawusse Agbessi, Marilyne Summo, and Francois Sabot
+#
+###################################################################################################################################
+
+
+
+use strict;
+use warnings;
+use Test::More 'no_plan'; 
+use Test::Deep;
+use fileConfigurator;
+
+
+#####################
+## PATH for datas test
+#####################
+
+# references files
+my $dataRefIrigin = "../DATA/Bank/referenceIrigin.fasta";
+my $dataRefArcad = "../DATA/Bank/referenceArcad.fasta";
+my $dataOneBam = "../DATA/testData/samBam/oneBam/";
+
+
+print "\n\n#################################################\n";
+print "#### TEST gatk UnifiedGenotyper\n";
+print "#################################################\n";
+
+# Remove files and directory created by previous test 
+my $testingDir="../DATA-TEST/oneBam-noSGE-otherBlocks";
+my $cleaningCmd="rm -Rf $testingDir";
+system ($cleaningCmd) and die ("ERROR: $0 : Cannot remove the previous test directory with the command $cleaningCmd \n$!\n");
+
+#Creating config file for this test
+my @listSoft = ("gatkUnifiedGenotyper");
+fileConfigurator::createFileConf(\@listSoft,"blockTestConfig.txt");
+
+my $runCmd = "toggleGenerator.pl -c blockTestConfig.txt -d ".$dataOneBam." -r ".$dataRefIrigin." -o ".$testingDir;
+print "\n### Toggle running : $runCmd\n";
+system("$runCmd") and die "#### ERROR : Can't run TOGGLE for gatkUnifiedGenotyper";
+
+# check final results
+print "\n### TEST Ouput list & content : $runCmd\n";
+my $observedOutput = `ls $testingDir/finalResults`;
+my @observedOutput = split /\n/,$observedOutput;
+my @expectedOutput = ('RC3-SAMTOOLSVIEW.GATKUNIFIEDGENOTYPER.vcf','RC3-SAMTOOLSVIEW.GATKUNIFIEDGENOTYPER.vcf.idx');
+
+# expected output test
+is_deeply(\@observedOutput,\@expectedOutput,'toggleGenerator - One Bam (no SGE) gatkUnifiedGenotyper file list ');
+
+# expected output content
+$observedOutput=`grep -v "#" $testingDir/finalResults/RC3-SAMTOOLSVIEW.GATKUNIFIEDGENOTYPER.vcf`; # We pick up only the position field
+chomp $observedOutput;
+is($observedOutput,"2233572	145	.	A	G	54.74	.	AC=2;AF=1.00;AN=2;DP=2;Dels=0.00;ExcessHet=3.0103;FS=0.000;HaplotypeScore=0.0000;MLEAC=2;MLEAF=1.00;MQ=49.84;MQ0=0;QD=27.37;SOR=2.303	GT:AD:DP:GQ:PL	1/1:0,2:2:6:82,6,0", 'toggleGenerator - One Bam (no SGE) gatkUnifiedGenotyper content');
+
+
