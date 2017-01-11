e988dfe78d591d0b1f0428b230f228dbc67f7e8e
sravel <sebastien.ravel@cirad.fr>
2017-01-11 08:50:04 +0100

    change die to warn for Block tests

--- TEST/all_tests.pl
+++ TEST/all_tests.pl
@@ -45,10 +45,10 @@ print "\n\n#################################################\n";
 print "#### INDIVIDUAL BLOCK TEST \n";
 print "#################################################\n";
 
-#system("perl assemblyBlock.pl") and die "ERROR: $0: Cannot run test for assemblyBlock.pl  \n$!\n";
-system("perl gatkBlock.pl") and die "ERROR: $0: Cannot run test for gatkBlock.pl  \n$!\n";
-system("perl picardtoolsBlock.pl") and die "ERROR: $0: Cannot run test for assemblyBlock.pl  \n$!\n";
-system("perl samtoolsBlock.pl") and die "ERROR: $0: Cannot run test for assemblyBlock.pl  \n$!\n";
+#system("perl assemblyBlock.pl") and warn "ERROR: $0: Cannot run test for assemblyBlock.pl  \n$!\n";
+system("perl gatkBlock.pl") and warn "ERROR: $0: Cannot run test for gatkBlock.pl  \n$!\n";
+system("perl picardtoolsBlock.pl") and warn "ERROR: $0: Cannot run test for assemblyBlock.pl  \n$!\n";
+system("perl samtoolsBlock.pl") and warn "ERROR: $0: Cannot run test for assemblyBlock.pl  \n$!\n";
 
 
 #####################
